8ecda05e2fe5f112126f4673d6285c0b7ad580a7
Juchar <Julien.Charpenel@Gmail.com>
2017-07-10 17:36:52 +0200

    Remove push again, heroku does not like it

--- md-stepper-demo/src/main/java/org/vaadin/addons/md_stepper/demo/DemoUI.java
+++ md-stepper-demo/src/main/java/org/vaadin/addons/md_stepper/demo/DemoUI.java
@@ -1,14 +1,11 @@
 package org.vaadin.addons.md_stepper.demo;
 
-import com.vaadin.annotations.Push;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.shared.communication.PushMode;
 import com.vaadin.shared.ui.MarginInfo;
-import com.vaadin.shared.ui.ui.Transport;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.Label;
@@ -21,7 +18,6 @@ import javax.servlet.annotation.WebServlet;
 
 @Theme("default")
 @Title("Stepper Add-on Demo")
-@Push(transport = Transport.LONG_POLLING)
 @SuppressWarnings("serial")
 public class DemoUI extends UI implements StepperPropertiesLayout.StepperCreateListener {
 
